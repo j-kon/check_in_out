96823c57119db9b6fe602ce0bb5c2044f9976414
Jeremiah Jacob <jaykon4me@gmail.com>
2019-11-25 13:36:53 +0100

    Initial commit

---
+++ android/app/src/main/kotlin/com/jaydroid/check_in_out/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.jaydroid.check_in_out
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
